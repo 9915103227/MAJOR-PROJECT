88b012d8ddaa8788303aeb4e7a0efc9500ac974e
Pranav Saxena <psaxena130@gmail.com>
2018-08-24 08:31:52 +0530

    Spark charts xls added

--- K_Means/kmeans.py
+++ K_Means/kmeans.py
@@ -26,7 +26,7 @@ y_train = sc_y.fit_transform(y_train)"""
 from sklearn.cluster import KMeans
 wcss = []
 for i in range(1, 11):
-    kmeans = KMeans(n_clusters = i, init = 'k-means++', random_state = 42)
+    kmeans = KMeans(n_clusters = i, init = 'k-means++', random_state = 42,max_iter=300)
     kmeans.fit(X)
     wcss.append(kmeans.inertia_)
 print(wcss)
@@ -37,15 +37,16 @@ plt.ylabel('WCSS')
 plt.show()
 
 # Fitting K-Means to the dataset
-kmeans = KMeans(n_clusters = 3, init = 'k-means++', random_state = 42)
+kmeans = KMeans(n_clusters = 6, init = 'k-means++', random_state = 42,max_iter=300)
 y_kmeans = kmeans.fit_predict(X)
 
 # Visualising the clusters
 plt.scatter(X[y_kmeans == 0, 0], X[y_kmeans == 0, 1], s = 100, c = 'red', label = 'Cluster 1')
 plt.scatter(X[y_kmeans == 1, 0], X[y_kmeans == 1, 1], s = 100, c = 'blue', label = 'Cluster 2')
 plt.scatter(X[y_kmeans == 2, 0], X[y_kmeans == 2, 1], s = 100, c = 'green', label = 'Cluster 3')
-#plt.scatter(X[y_kmeans == 3, 0], X[y_kmeans == 3, 1], s = 100, c = 'cyan', label = 'Cluster 4')
-#plt.scatter(X[y_kmeans == 4, 0], X[y_kmeans == 4, 1], s = 100, c = 'magenta', label = 'Cluster 5')
+plt.scatter(X[y_kmeans == 3, 0], X[y_kmeans == 3, 1], s = 100, c = 'cyan', label = 'Cluster 4')
+plt.scatter(X[y_kmeans == 4, 0], X[y_kmeans == 4, 1], s = 100, c = 'magenta', label = 'Cluster 5')
+plt.scatter(X[y_kmeans == 5, 0], X[y_kmeans == 5, 1], s = 100, c = 'black', label = 'Cluster 6')
 plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s = 300, c = 'yellow', label = 'Centroids')
 plt.title('Clusters of customers')
 plt.xlabel('Annual Income (k$)')
